30ad8b8d341f35d28bc837a9b6891a797edd3c7a
Nahid HK <nahidappltd@gmail.com>
2026-01-25 17:13:53 +0600

    The Add New Commits To add the New pythin Syntext sytem

---
+++ W3school/for_in_loop.py
@@ -0,0 +1,16 @@
+# This is a Very Smart systme . 
+#Looping Through a String
+"""
+for x in "Nahid":
+  print(x)
+"""
+
+# String Length
+"""
+a = "Hello, Word!"
+print(len(a))
+"""
+
+# Check String , Check the Filter if Else Text tyoeing 
+txt = "Hello I'm Nahidul islam!"
+ptint("Hello" in txt);
